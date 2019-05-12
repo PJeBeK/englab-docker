06d08236aa85619e1abaafd90c41de2562d5cd83
Peyman <peyman.jabarzade@gmail.com>
2019-05-12 16:28:58 +0430

    fix bug of rendering template

--- app/app.py
+++ app/app.py
@@ -1,7 +1,10 @@
-from flask import Flask, request
+from flask import Flask, render_template
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def root():
-    return app.send_static_file('index.html')
+	return render_template('index.html')
+
+if __name__ == '__main__':
+	app.run()
